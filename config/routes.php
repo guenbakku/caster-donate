a82d058165d65a856537f7c6b75b160bc3a4ad49
Nguyen Van Bach <vanbach1213@gmail.com>
2017-10-01 09:11:29 +0900

    Sửa lại cấu trúc của api controller

--- config/routes.php
+++ config/routes.php
@@ -80,6 +80,10 @@ Router::scope('/', function (RouteBuilder $routes) {
     $routes->fallbacks(DashedRoute::class);
 });
 
+Router::prefix('api/v1', function ($routes) {
+    $routes->fallbacks(DashedRoute::class);
+});
+
 /**
  * Load all plugin routes. See the Plugin documentation on
  * how to customize the loading of plugin routes.
--- src/Controller/Api/V1/TagsController.php
+++ src/Controller/Api/V1/TagsController.php
@@ -1,27 +1,32 @@
 <?php
-namespace App\Controller;
+namespace App\Controller\Api\V1;
 
 use Cake\Event\Event;
 use App\Controller\AppController;
 use App\Model\Logic\Profile\Profile;
 use App\Model\Logic\Tag\Tag;
 
-class AjaxListenerController extends AppController
+class TagsController extends AppController
 {
     public function beforeFilter(Event $event)
     {
         parent::beforeFilter($event);
         $this->Auth->allow();
+        $this->eventManager()->off($this->Csrf);
         $this->autoRender = false;
+        $this->viewBuilder()->layout('ajax');
         $this->response->charset('UTF-8');
         $this->response->type('json');
     }
 
     public function index()
     {
+        // Trả về tất cả tag đang có trong db
+        // Format của dữ liệu trả về tham khảo tại: 
+        // http://demos.telerik.com/kendo-ui/multiselect/serverfiltering
     }
 
-    public function AutoCompleteTag($keyword)
+    public function search($keyword)
     {
         $Tag = new Tag();
         if ($this->request->is("ajax")) {
