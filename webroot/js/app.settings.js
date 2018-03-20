f453f5096ef8a94d04d4b556e3d2e1d31d5004e3
Ngo Van Thinh <nvthinh.contact@gmail.com>
2018-03-20 21:26:39 +0700

    Thêm plugin SweetAlert dùng để hiển thị thông báo

--- webroot/js/app.settings.js
+++ webroot/js/app.settings.js
@@ -36,3 +36,88 @@ function boldingKeyword(key,text)
     */
     return text.replace(new RegExp(key,'i'), '<strong>$&</strong>');
 }
+
+
+/*
+**  SWEETALERT
+** >>>>>>>>>>>>>>>>>>>>>START
+*/
+function swalAutoClose(title, timer = 1500 ,text = '')
+{
+    swal({   
+        title: title,   
+        text: text,   
+        timer: timer,   
+        showConfirmButton: false 
+    });
+}
+
+function swalBasic(message = '')
+{
+    swal(message);
+}
+
+function swalTitle(title = '', message = '')
+{
+    swal(title, message);
+}
+
+function swalSuccess(title = '', message= '')
+{
+    swal(title, message, 'success');
+}
+
+function swalError(title = '', message= '')
+{
+    swal(title, message, 'error');
+}
+
+function swalWarning(title = '', message = '', confirmButtonText = 'Yes', successTitle = '', successMessage = '')
+{
+    swal({   
+        title: title,   
+        text: message,   
+        type: "warning",   
+        showCancelButton: true,   
+        confirmButtonColor: "#DD6B55",   
+        confirmButtonText: confirmButtonText,   
+        closeOnConfirm: false 
+    }, function(){   
+        swalSuccess(successTitle, successMessage); 
+    });
+}
+
+function swalParams(title = '', message = '', confirmButtonText = 'Yes', cancelButtonText = 'No', successTitle = '', successMessage = '', canceledTitle = '', canceledMessage = '')
+{
+    swal({   
+        title: title,   
+        text: message,   
+        type: "warning",   
+        showCancelButton: true,   
+        confirmButtonColor: "#DD6B55",   
+        confirmButtonText: confirmButtonText,   
+        cancelButtonText: cancelButtonText,   
+        closeOnConfirm: false,   
+        closeOnCancel: false 
+    }, function(isConfirm){   
+        if (isConfirm) {     
+            swalSuccess(successTitle, successMessage);   
+        } else {     
+            swalError(canceledTitle, canceledMessage, "error");   
+        } 
+    });
+}
+
+function swalImage(title = '', message = '', imageUrl = false)
+{
+    swal({   
+        title: title,   
+        text: message,   
+        imageUrl: imageUrl 
+    });
+}
+
+/**
+ * SWEETALERT
+ * END<<<<<<<<<<<<<<<<<<<<<<<<<<
+ */
