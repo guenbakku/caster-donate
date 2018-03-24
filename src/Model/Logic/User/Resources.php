e1f3d5faaf0a4dbcee8eff41be8b0f119a320511
Ngo Van Thinh <nvthinh.contact@gmail.com>
2018-03-24 12:28:57 +0700

    Sửa logic Resources

--- src/Model/Logic/User/Resources.php
+++ src/Model/Logic/User/Resources.php
@@ -13,15 +13,27 @@ class Resources
         $this->resourcesTb = TableRegistry::get('resources');
     }
 
-    public function get($user_id)
+    public function getAllAvailableResources($user_id, $type = '')
     {
-        $resources = $this->resourcesTb->find()
-        ->where(['user_id is' => null])
-        ->orwhere(['user_id =' => $user_id])
-        ->all();
+        $codeHelper = new CodeHelper(new \Cake\View\View());
+        $type_id = $codeHelper->setTable('resource_types')->getKey($type,'id');
+        
+        $resources = $this->resourcesTb->find();
+        
+        if($type_id != null)
+        {
+            
+        }
+        $resources = $resources->andWhere([
+            'OR' =>[
+                'user_id is' => null,//public
+                'user_id =' => $user_id//private
+            ]
+        ])->order(['user_id'=>'DESC'])->all();
 
         if ($resources) {
-            //
+            //convert resource's path
+            $resources  = $this->convertResourePath($resources);
         }
         else {
             $resources = $this->resourcesTb->newEntity();
@@ -29,10 +41,21 @@ class Resources
         return $resources;
     }
 
+    public function getAllPrivateResources($user_id, $type = '')
+    {
+       
+    }
+
+    public function getAllPublicResources()
+    {
+       
+    }
+
     public function uploadNew($user_id, array $new_resource)
     {
         $codeHelper = new CodeHelper(new \Cake\View\View());
         $resource = $this->resourcesTb->newEntity();
+        $resource['user_id'] = $user_id;
 
         //image
         if($new_resource['resource_type_id'] == $codeHelper->setTable('resource_types')->getKey('image','id')){
@@ -44,7 +67,7 @@ class Resources
             $path = Configure::read('System.Paths.resource_dir.audio');
             $validate = 'audio';
         }
-
+        
         $this->resourcesTb->patchEntity($resource, $new_resource,[
             'validate' => $validate,
         ]);
@@ -58,11 +81,32 @@ class Resources
                     ],
                 ]);
             }
-
+            
             $this->resourcesTb->save($resource,['validate' => false]);
         }
 
         return $resource;
+    }
+
+    private function convertResourePath($resources)
+    {
+        $codeHelper = new CodeHelper(new \Cake\View\View());
+        $image_type_id = $codeHelper->setTable('resource_types')->getKey('image','id');
+        $audio_type_id = $codeHelper->setTable('resource_types')->getKey('audio','id');
+        foreach($resources as $key => $resource)
+        {
+            if($resource['resource_type_id'] == $image_type_id){
+                $resource['filename'] = str_replace('webroot/', '', Configure::read('System.Paths.resource_dir.image').'/'.$resource['filename']);
+            }
+            //audio
+            elseif($resource['resource_type_id'] == $audio_type_id){
+                $resource['filename'] = str_replace('webroot/', '', Configure::read('System.Paths.resource_dir.audio').'/'.$resource['filename']);
+            }else{
+                $path = '';
+            }
+        }
+        
+        return $resources;
     }
 }
 ?>
