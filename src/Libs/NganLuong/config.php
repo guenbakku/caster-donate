1663c046aee66c4225fd82fdd6cec12c26a12ad6
Ngo Van Thinh <nvthinh.contact@gmail.com>
2018-05-26 08:22:46 +0700

    Thư viện Ngân Lượng

---
+++ src/Libs/NganLuong/config.php
@@ -0,0 +1,11 @@
+<?php
+	header("Content-Type: text/html; charset=utf-8");	
+	define('URL_WS','https://sandbox.nganluong.vn:8088/nl30/micro_checkout_api.php?wsdl');	//Môi trường test của ngân lượng
+	define('RECEIVER','nvthinh.contact@gmail.com'); // Email tài khoản Ngân Lượng
+	define('MERCHANT_ID', '46089'); // Mã kết nối
+	define('MERCHANT_PASS', 'ecb2dfc1a18f48b23cdbc1c5aca606d3'); // Mật khẩu kết nối
+	
+?>
+
+
+	
